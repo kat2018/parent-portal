45b084f8e4b3449136e3a0b1fec3bcd822052d6d
kat2018 <kat.vge@gmail.com>
2018-06-07 13:35:40 -0400

    updated seeds and created activities Schema

--- app.js
+++ app.js
@@ -12,7 +12,19 @@ require("dotenv").config();
 
 //mongoose
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI); 
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost/parent-portal");
+}
+mongoose.connection.on("error", function(err) {
+  console.error("MongoDB connection error: " + err);
+  process.exit(-1);
+});
+mongoose.connection.once("open", function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,21 +39,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Connect to database
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-  mongoose.connect("mongodb://heroku_x2h43kh5:oc3c3os03ud2r7i5rub4o1i94m@ds143181.mlab.com:43181/heroku_x2h43kh5");
-}
-mongoose.connection.on('error', function (err) {
-  console.error('MongoDB connection error: ' + err);
-  process.exit(-1);
-}
-);
-mongoose.connection.once('open', function () {
-  console.log("Mongoose has connected to MongoDB!");
-});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
